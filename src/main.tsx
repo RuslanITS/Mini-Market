790d1208ea706ae12a8413be465240fb4c8e1d4d
Ruslan <rcernov442@gmail.com>
2026-06-06 17:47:56 +0600

    deleted the empty index.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client'
 import { ToastContainer } from "react-toastify";
 import { BrowserRouter } from 'react-router'
 import "bootstrap/dist/css/bootstrap.min.css";
-import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
